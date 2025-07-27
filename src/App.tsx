d9977100d0ad4acd84e624f4ec67eb7955e42c25
Mir Hussain <mirhussainmurtaza@gmail.com>
2025-07-27 20:47:59 +0600

    add: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import { Outlet } from "react-router";
 import CommonLayout from "./components/layout/CommonLayout";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./components/ui/dropdown-menu";
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <CommonLayout>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem>Light</DropdownMenuItem>
+          <DropdownMenuItem>Dark</DropdownMenuItem>
+          <DropdownMenuItem>System</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
       <Outlet />
     </CommonLayout>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/index.tsx";
+import { ThemeProvider } from "./providers/theme.provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
